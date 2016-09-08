3a11a285c8f830a8a6e8da39a6a1421a9b3170f1
smcnitt <steven.codes.in.ruby@gmail.com>
2016-09-07 18:51:12 -0600

    add Categories dynamically to index.html

--- populate_rango.py
+++ populate_rango.py
@@ -7,13 +7,26 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
-    python_pages =  {"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/"},{"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/"},{"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/"}  
-     
-    django_pages = [{"title":"Official Django Tutorial","url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},{"title":"Django Rocks","url":"http://www.djangorocks.com/"},{"title":"How to Tango with Django","url":"http://www.tangowithdjango.com/"}]
-     
-    other_pages  = [{"title":"Bottle", "url":"http://bottlepy.org/docs/dev/"},{"title":"Flask", "url":"http://flask.pocoo.org"}]
-      
-    cats         = {"Python": {"pages": python_pages}, "Django": {"pages": django_pages},"Other Frameworks": {"pages": other_pages} }
+    python_pages = [
+        {"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/", "views": 32},
+        {"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/", "views": 16},
+        {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/", "views": 8},  ]
+    
+    django_pages = [
+        {"title":"Official Django Tutorial",
+            "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 32},
+        {"title":"Django Rocks",
+            "url":"http://www.djangorocks.com/" , "views": 16},
+        { "title":"How to Tango with Django",
+            "url":"http://www.tangowithdjango.com/", "views": 8 } ]
+    
+    other_pages = [
+        { "title":"Bottle", "url":"http://bottlepy.org/docs/dev/", "views": 32},
+        { "title":"Flask", "url":"http://flask.pocoo.org", "views": 16} ]
+    
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16} }
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
@@ -32,8 +45,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0, likes=0):
+    c       = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,15 @@
 from django.http      import HttpResponse
 from django.shortcuts import render
+# Import the Category model.
+from rango.models     import Category
 
 # Create your views here.
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+	# Query the db for a list of all categories stored.
+	# Order the categories by number of likes in descending order.
+	# Retrieve the top 5 only - or all if less than 5.
+    category_list = Category.objects.order_by('-likes')[:5]
+    # Construct a dictionary with context_dict to pass to the template engine as its context.
+    context_dict = {'categories': category_list}
+    # Return a rendered response to send to the client(i.e. web browser).
+    return render(request, 'rango/index.html', context_dict)
